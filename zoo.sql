e74d32d3566b7a97d668e3d85cce74e35f4fbe81
Lichapa <lichapau@gmail.com>
2020-11-19 16:15:48 +0200

    finish 2, SELECT FROM world

--- zoo.sql
+++ zoo.sql
@@ -100,5 +100,83 @@
   /*Give the name and the per capita GDP for those countries with a 
   population of at least 200 million.  */
   SELECT name, gdp/population AS per_capita
-FROM world
-WHERE population > 200000000
+  FROM world
+  WHERE population > 200000000
+
+  -- 2.4 South America in Millions
+  /*Show the name and population in millions for the countries of the continent 'South America'. 
+  Divide the population by 1000000 to get population in millions. */
+
+  SELECT name, population/1000000
+  FROM world
+  WHERE continent = 'south america'
+
+  -- 2.5 France, Germany, Italy
+  /* Show the name and population for France, Germany, Italy  */
+
+  SELECT name, population
+  FROM world
+  WHERE name IN ('france', 'germany','italy')
+
+  --2.6 UNITED
+  /* Show the name and population for France, Germany, Italy  */
+  SELECT name
+  FROM world
+  WHERE name LIKE '%UNITED%'
+
+  -- 2.7 Two Ways to be big
+  /* Show the countries that are big by area or big by population. Show name, 
+  population and area.  */
+
+  SELECT name, population, area 
+  FROM world
+  WHERE area >= 3000000
+  OR population >= 250000000
+
+  -- 2.8 One or the other (but not both)
+  /* Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) 
+  or big by population (more than 250 million) but not both. Show name, population and area. */
+
+  SELECT name, population, area
+  FROM world
+  WHERE population >= 250000000
+  XOR area >= 3000000
+
+  -- 2.9 Rounding
+  /* For South America show population in millions and GDP in billions both to 2 decimal places. */
+
+  SELECT name, ROUND(population/1000000,2), ROUND(gdp/1000000000,2)
+  FROM world
+  WHERE continent = 'south america'
+
+  -- 2.10 Trillion dollar economies
+  /* Show per-capita GDP for the trillion dollar countries to the nearest $1000. */
+  SELECT name, ROUND(gdp/population, -3 ) AS per_capita
+  FROM world
+  WHERE GDP > 1000000000000
+
+  -- 2.11 Name and capital have the same length
+  /* Show the name and capital where the name and the capital have the same number of characters. */
+  SELECT name, capital 
+  FROM world
+  WHERE LENGTH(name) = LENGTH(capital)
+
+  -- 2.12 Matching name and capital
+  /* Show the name and the capital where the first letters of each match. 
+  Don't include countries where the name and the capital are the same word.*/
+
+  SELECT name, capital
+  FROM world
+  WHERE LEFT(name,1) = LEFT(capital,1) 
+  AND name != capital
+
+  -- 2.13 All the vowels
+  /* Find the country that has all the vowels and no spaces in its name. */
+
+  SELECT name FROM world
+  WHERE name LIKE '%a%' AND 
+  name  LIKE '%e%' AND 
+  name  LIKE '%i%' AND 
+  name  LIKE '%o%' AND 
+  name  LIKE '%u%' AND 
+  name NOT LIKE '% %'
