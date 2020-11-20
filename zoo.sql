9849d412afe7c85e381700bf1dd47466cd817205
Lichapa <lichapau@gmail.com>
2020-11-20 12:38:29 +0200

    Complete tutorial 3 Add Tutorial 4, SELECT  within SELECT

--- zoo.sql
+++ zoo.sql
@@ -181,4 +181,129 @@
   name  LIKE '%u%' AND 
   name NOT LIKE '% %'
 
---3 SELECT FROM nobel
+-- 3 SELECT FROM nobel
+  -- 3.1 Winners from 1950
+  /* Change the query shown so that it displays Nobel prizes for 1950. */
+  SELECT yr, subject, winner
+  FROM nobel
+  WHERE yr = 1950
+
+  -- 3.2 1962 Literature
+  /* Show who won the 1962 prize for Literature.  */
+  SELECT winner
+  FROM nobel
+  WHERE yr = 1962
+  AND subject = 'Literature';
+
+  -- 3.3 Albert Einstein
+  /* Show the year and subject that won 'Albert Einstein' his prize.  */
+  SELECT yr, subject
+  FROM nobel
+  WHERE winner = 'Albert Einstein'
+
+  -- 3.4 Recent Peace Prizes
+  /* Give the name of the 'Peace' winners since the year 2000, including 2000.  */
+  SELECT winner
+  FROM nobel
+  WHERE subject = 'peace' 
+  AND yr >= 2000;
+
+  --3.5 Literature in the 1980's
+  /* Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive. */
+  SELECT *
+  FROM nobel
+  WHERE subject = 'literature'
+  AND yr BETWEEN 1980 AND 1989;
+
+  -- 3.6 Only Presidents
+  /* Show all details of the presidential winners:
+
+    Theodore Roosevelt
+    Woodrow Wilson
+    Jimmy Carter
+    Barack Obama */
+
+  SELECT * FROM nobel
+  WHERE winner IN
+    ('Theodore Roosevelt',
+    'Woodrow Wilson',
+    'Jimmy Carter',
+    'Barack Obama');
+
+  -- 3.7 John
+  /* Show the winners with first name John */
+  SELECT winner
+  FROM nobel
+  WHERE winner LIKE 'John%';
+
+  -- 3.8 Chemistry and Physics from different years
+  /* Show the year, subject, and name of Physics winners for 1980 
+    together with the Chemistry winners for 1984.*/
+
+  SELECT yr, subject, winner
+  FROM nobel
+  WHERE yr = 1980 AND subject = 'Physics'
+  OR yr = 1984 AND  subject = 'chemistry';
+
+  -- 3.9 Exclude Chemists and Medics
+  /* Show the year, subject, and name of winners for 1980 excluding Chemistry and Medicine */
+  SELECT yr, subject, winner
+  FROM nobel
+  WHERE yr = 1980 AND
+  subject != 'chemistry' AND
+  subject != 'Medicine';
+
+  --3.10 Early Medicine, Late Literature
+  /* Show year, subject, and name of people who won a 'Medicine' prize in an early year 
+    (before 1910, not including 1910) together with winners of a 'Literature' prize in a 
+    later year (after 2004, including 2004) */
+  SELECT yr, subject, winner
+  FROM nobel
+  WHERE subject = 'Medicine' AND yr < 1910
+  OR subject = 'literature' AND yr >= 2004;
+
+  -- 3.11 Umlaut
+  /* Find all details of the prize won by PETER GRÜNBERG */
+  SELECT *
+  FROM nobel
+  WHERE winner LIKE 'peter gr%'
+
+  -- 3.12 Apostrophe
+  /* Find all details of the prize won by EUGENE O'NEILL */
+  SELECT *
+  FROM nobel
+  WHERE winner = 'Eugene O''neill'
+
+  -- 3.13 Knights of the realm
+  /* List the winners, year and subject where the winner starts with Sir. 
+  Show the the most recent first, then by name order. */
+  SELECT winner, yr, subject
+  FROM nobel
+  WHERE winner LIKE 'sir%'
+  ORDER BY yr DESC
+
+  -- 3.14 Chemistry and Physics last
+  /* Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last. */
+
+  SELECT winner, subject
+  FROM nobel
+  WHERE yr=1984
+  ORDER BY subject IN ('Chemistry', 'physics'),subject, winner
+
+-- 4 SELECT within SELECT 
+  -- 4.1 Bigger than Russia
+  /* List each country name where the population is larger than that of 'Russia'. */
+
+  SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
+
+  -- 4.2 Richer than UK
+  /* Show the countries in Europe with a per capita GDP greater than 'United Kingdom'. */
+  SELECT name
+  FROM world
+  WHERE continent = 'Europe' AND GDP/population > 
+        (SELECT gdp/population
+         FROM world
+         WHERE name = 'united Kingdom')
